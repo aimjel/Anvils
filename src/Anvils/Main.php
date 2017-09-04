6e46ddf8b70329df8811b9bb972896fc5c7d2535
Angel <bubblicious712@gmail.com>
2017-09-03 22:38:33 -0400

    Update Main.php

--- src/Anvils/Main.php
+++ src/Anvils/Main.php
@@ -2,7 +2,37 @@
 
 use pocketmine\Server;
 use pocketmine\Player;
+use pocketmine\item\Item;
 use pocketmine\plugin\PluginBase;
+use pocketmine\scheduler\PluginTask;
+
 
 class Main extends PluginBase{
+  
+  public $chechks = [];
+  
+  public function onEnable(){
+    $this->getServer()->getScheduler()->scheduleRepeatingTask(new Checker($this), 0);
+  }
+}
 
+class Checker extends PluginTask{
+  
+  public function __construct(Main $plugin){
+    $this->plugin = $plugin;
+  }
+  
+  public function getPlugin(){
+    return $this->plugin;
+  }
+  
+  public function onRun(int $currentTick){
+    $this->getOwner();
+    foreach($this->getPlugin()->getServer()->getOnlinePlayers() as $players){
+      $player = $players;
+      if($player->getItemInHand() !== null){
+        $player->getItemInHand()->setCustomName($player->getItemInHand()->getCustomName());
+      }
+    }
+  }
+}
